f039299e89c02df92c0e8df59914fdc36ff92e58
Erik Price <github@erikprice.net>
2020-05-25 12:16:20 -0700

    Initial import

---
+++ custom.el
@@ -0,0 +1,64 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#373b41"))
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+   (quote
+    ("850213aa3159467c21ee95c55baadd95b91721d21b28d63704824a7d465b3ba8" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(fci-rule-color "#373b41")
+ '(flycheck-executable-find (quote executable-find))
+ '(flycheck-go-build-executable "/home/erik/.gimme/versions/go1.12.7.linux.amd64/bin/go")
+ '(flycheck-go-gofmt-executable "/home/erik/.gimme/versions/go1.12.7.linux.amd64/bin/gofmt")
+ '(flycheck-rust-cargo-executable "/home/erik/.cargo/bin/cargo")
+ '(flycheck-rust-executable "/home/erik/.cargo/bin/rustc")
+ '(global-flycheck-mode t)
+ '(go-command "/home/erik/.gimme/versions/go1.12.7.linux.amd64/bin/go")
+ '(haskell-mode-stylish-haskell-path "~/.local/bin/stylish-haskell")
+ '(haskell-stylish-on-save t)
+ '(magit-blame-echo-style (quote margin))
+ '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (ido-completing-read+ pyvenv typescript-mode nord-theme go-guru quelpa-use-package quelpa flycheck-rust flycheck go-mode rust-mode js2-mode js2 color-theme-sanityinc-tomorrow mu4e mu smartparens ido-ubiquitous elm-mode psci psc-ide purescript-mode hindent hindent-mode ag markdown-mode ts projectile winner-mode smex zenburn haskell-mode magit evil use-package)))
+ '(rust-cargo-bin "/home/erik/.cargo/bin/cargo")
+ '(rust-format-on-save t)
+ '(rust-rustfmt-bin "/home/erik/.cargo/bin/rustfmt")
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#cc6666")
+     (40 . "#de935f")
+     (60 . "#f0c674")
+     (80 . "#b5bd68")
+     (100 . "#8abeb7")
+     (120 . "#81a2be")
+     (140 . "#b294bb")
+     (160 . "#cc6666")
+     (180 . "#de935f")
+     (200 . "#f0c674")
+     (220 . "#b5bd68")
+     (240 . "#8abeb7")
+     (260 . "#81a2be")
+     (280 . "#b294bb")
+     (300 . "#cc6666")
+     (320 . "#de935f")
+     (340 . "#f0c674")
+     (360 . "#b5bd68"))))
+ '(vc-annotate-very-old-color nil)
+ '(visible-bell nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "JetBrains Mono" :foundry "JB  " :slant normal :weight normal :height 102 :width normal))))
+ '(go-guru-hl-identifier-face ((t (:inherit highlight :underline t)))))
---
+++ init.el
@@ -0,0 +1,237 @@
+(require 'package)
+
+(add-to-list 'load-path
+             (expand-file-name "~/.emacs.d/vendor"))
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (require 'quelpa nil t)
+  (with-temp-buffer
+    (url-insert-file-contents "https://framagit.org/steckerhalter/quelpa/raw/master/bootstrap.el")
+    (eval-buffer)))
+
+;; Updating quelpa at startup takes FOREVER.
+(setq-default quelpa-checkout-melpa-p nil)
+
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+
+(require 'quelpa-use-package)
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package evil
+  :ensure t
+
+  :init
+  (setq evil-split-window-below t
+        evil-vsplit-window-right t
+        ;; bind Y to y$
+        evil-want-Y-yank-to-eol t
+
+        ;; NOTE: Temporary, let's see if this is nice.
+        evil-insert-state-cursor '(bar "White")
+        evil-normal-state-cursor '(box "White")
+        evil-visual-state-cursor '(box "#F86155"))
+
+  :config
+  (evil-mode)
+
+  ;; Ergonomic bindings
+  (define-key evil-normal-state-map (kbd "C-w C-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-w C-j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-w C-k") 'evil-window-up)
+  (define-key evil-normal-state-map (kbd "C-w C-l") 'evil-window-right)
+
+  ;; Winner-mode ish bindings.
+  (global-set-key (kbd "S-<left>") 'evil-window-left)
+  (global-set-key (kbd "S-<down>") 'evil-window-down)
+  (global-set-key (kbd "S-<up>") 'evil-window-up)
+  (global-set-key (kbd "S-<right>") 'evil-window-right))
+
+(use-package magit
+  :ensure t
+
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status))
+
+(use-package haskell-mode
+  :ensure hindent
+
+  :init
+  (defun erik/haskell-mode-hook ()
+   (progn
+    (subword-mode +1)
+    (eldoc-mode +1)
+    (haskell-indentation-mode +1)
+    (haskell-doc-mode +1)))
+
+  :config
+  (add-hook 'haskell-mode-hook 'erik/haskell-mode-hook))
+
+(use-package ido
+  :ensure ido-completing-read+
+
+  :init
+  (setq ido-enable-prefix nil
+        ido-enable-flex-matching t
+        ido-create-new-buffer 'always)
+
+  :config
+  (ido-mode t)
+  (ido-everywhere t)
+  (require 'ido-completing-read+)
+  (ido-ubiquitous-mode 1))
+
+(use-package smex
+  :ensure t
+
+  :config
+  (smex-initialize)
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands))
+
+(use-package winner
+  :ensure t
+
+  :config
+  (winner-mode +1))
+
+(use-package projectile
+  :ensure ag
+
+  :config
+  (projectile-mode)
+  (global-set-key (kbd "C-c p s s") 'projectile-ag))
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package elm-mode
+  :ensure t
+
+  :config
+  (add-hook 'elm-mode-hook 'subword-mode))
+
+(use-package smartparens
+  :ensure t
+
+  :config
+  (require 'smartparens-config)
+  (smartparens-global-mode)
+  (sp-use-paredit-bindings))
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t)
+
+(use-package js2-mode
+  :ensure t
+
+  :config
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (setq js2-basic-offset 2))
+
+(use-package rust-mode
+  :ensure t)
+
+(use-package go-mode
+  :ensure go-guru
+
+  :config
+  (add-hook 'before-save-hook #'gofmt-before-save)
+  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode))
+
+
+(use-package flycheck
+  :ensure flycheck-rust
+  :quelpa t
+
+  :config
+  (flycheck-define-checker sql-squabble
+    "A SQL syntax checker using the squabble tool.
+See URL `https://github.com/erik/squabble'."
+    :command ("squabble" "--reporter=sqlint" "--preset=full")
+    :standard-input t
+    :error-patterns
+    ((warning line-start "stdin:" line ":" column ":WARNING "
+              (message (one-or-more not-newline)
+                       (zero-or-more "\n"))
+              line-end)
+     (error line-start "stdin:" line ":" column ":ERROR "
+            (message (one-or-more not-newline)
+                     (zero-or-more "\n"))
+            line-end))
+    :modes (sql-mode))
+
+  (add-to-list 'flycheck-checkers 'sql-squabble)
+  (flycheck-add-next-checker 'python-flake8 'python-mypy t))
+
+
+(use-package flycheck-rust
+  :ensure t
+  :quelpa t
+
+  :init
+  (with-eval-after-load 'rust-mode
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+
+(use-package nord-theme
+  :ensure t)
+
+(use-package typescript-mode
+  :ensure t)
+
+(use-package pyvenv
+  :ensure t)
+
+;; Custom things
+;; TODO: Split these out
+
+(require 'better-defaults)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(setq
+  make-backup-files nil
+  inhibit-startup-screen t
+  inhibit-startup-echo-area-message t
+
+  ;; Scroll view by lines, not pages.
+  scroll-conservatively 1000)
+
+(column-number-mode +1)
+
+;; Typing "yes" is too many characters, switch to "y"
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; I always accidentally C-x C-c somehow...
+(setq confirm-kill-emacs 'yes-or-no-p)
+
+;; mu4e is special and installs itself from outside of melpa
+;;
+;; (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+;;
+;; (when (require 'mu4e nil t)
+;;   (setq mu4e-maildir "~/mail"))
+
+;; cleanup whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(defun erik/font-lock-comments ()
+  "Add highlighting for TODO, FIXME, etc."
+  (font-lock-add-keywords
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|HACK\\|XXX\\|NOTE\\):"
+          1 font-lock-warning-face t))))
+
+(add-hook 'prog-mode-hook 'erik/font-lock-comments)
